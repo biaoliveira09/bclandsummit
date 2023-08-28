9d482226622c83f722b0957b9a7b5ef43aee48e7
Bia Oliveira <pellegrini.beatriz@gmail.com>
2023-08-28 11:22:47 -0700

    Change font to Futura

--- functions.php
+++ functions.php
@@ -183,3 +183,8 @@ function google_fonts() {
     wp_enqueue_style( 'poppins-font', "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap", false );
 }
 add_action( 'wp_enqueue_scripts', 'google_fonts' );
+
+function enqueue_custom_font(){
+    wp_enqueue_style( 'typekit-font', 'https://use.typekit.net/lox7jir.css');
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_custom_font');
