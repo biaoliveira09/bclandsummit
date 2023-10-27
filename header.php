93d1ebbb1d4b6c457bae84d1e62a321bfab4c17e
Bia Oliveira <pellegrini.beatriz@gmail.com>
2023-10-26 18:16:17 -0700

    Add GTag manager code

--- header.php
+++ header.php
@@ -13,9 +13,19 @@
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
+	<!-- Google tag (gtag.js) -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-LS148GZQMQ"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-LS148GZQMQ');
+		</script>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	
 
 	<?php wp_head(); ?>
 </head>
